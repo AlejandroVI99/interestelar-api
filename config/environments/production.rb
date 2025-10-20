dde021a026e7f69e40cbcddf32c70995459bd210
AlejandroVI99 <avi200599@gmail.com>
2025-10-20 14:49:27 -0600

    Refactor production configuration to enable public file server and adjust logging level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
 
   # Cache assets for far-future expiry since they are all digest stamped.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
@@ -37,7 +35,9 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Change to "debug" to log everything
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Change to "debug" to log everything (including potentially personally-identifiable information!)
+  config.log_level = :info
 
   # Prevent health checks from clogging up the logs.
   config.silence_healthcheck_path = "/up"
@@ -55,8 +55,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
+  # Enable locale fallbacks for I18n
   config.i18n.fallbacks = true
 
   # Do not dump schema after migrations.
